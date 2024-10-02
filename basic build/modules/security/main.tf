402a460e13a76294c0793f1ec17e63ad0713c4cf
C. Sanders <112976451+curthay@users.noreply.github.com>
2024-10-02 14:35:00 -0500

    Add files via upload

---
+++ basic build/modules/security/main.tf
@@ -0,0 +1,28 @@
+resource "aws_security_group" "main" {
+  name        = "main"
+  description = "Allow inbound traffic"
+  vpc_id      = var.vpc_id
+
+  dynamic "ingress" {
+    iterator = port
+    for_each = var.ingressrules
+    content {
+      from_port   = port.value
+      to_port     = port.value
+      protocol    = "tcp"
+      cidr_blocks = ["0.0.0.0/0"]
+    }
+
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "main_sg"
+  }
+}
